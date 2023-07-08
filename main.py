00e68bdb38341951898d1caf4deff1d6eafa776e
Madhur-01 <madhurgarg9926@gmail.com>
2023-07-08 17:04:39 +0530

    done

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ stocks = ('GOOG', 'AAPL', 'MSFT', 'GME')
 
 selected_stock = st.selectbox('Select dataset for prediction', stocks)
 
-n_years = st.slider(('Years of prediction:',1,4)
+n_years = st.slider('Years of prediction:',1,4)
 period = n_years*365
 
 @st.cache
