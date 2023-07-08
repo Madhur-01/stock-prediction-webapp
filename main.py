2720daefa9edb579c0347b1b26de9c0aa27a9ada
Madhur-01 <madhurgarg9926@gmail.com>
2023-07-08 17:25:00 +0530

    done

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ def plot_raw_data():
 plot_raw_data()
 
 #Predict forecast with Prophet
-df_train = data[["Data","Close"]]
+df_train = data[["Date","Close"]]
 df_train = df_train.rename(columns={"Date":"ds","Close":"y"})
 
 m = Prophet()
