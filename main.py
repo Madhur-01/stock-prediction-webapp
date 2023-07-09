83f1dbbbff33543415f311214f6994592af4be4b
Madhur-01 <madhurgarg9926@gmail.com>
2023-07-09 09:36:56 +0530

    done

--- main.py
+++ main.py
@@ -7,11 +7,16 @@ Created on Sat Jul  8 13:02:37 2023
 
 import streamlit as st
 from datetime import date
-
+import numpy
+import pandas
 import yfinance as yf
 from prophet import Prophet
 from prophet.plot import plot_plotly
 from plotly import graph_objs as go
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+
 
 START = "2015-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
@@ -24,6 +29,8 @@ selected_stock = st.selectbox('Select dataset for prediction', stocks)
 
 n_years = st.slider('Years of prediction:',1,5)
 period = n_years*365
+
+forecast_method = st.selectbox("Select forecasting method", ["Prophet", "LSTM"])
 
 @st.cache(persist=True, allow_output_mutation=True)
 
@@ -53,28 +60,63 @@ def plot_raw_data():
     
 plot_raw_data()
 
-#Predict forecast with Prophet
-df_train = data[["Date","Close"]]
-df_train.dropna(inplace=True)
-df_train = df_train.rename(columns={"Date":"ds","Close":"y"})
+if forecast_method == "LSTM":
+    df_train_lstm = data[["Date", "Close"]]
+    df_train_lstm.dropna(inplace=True)
+    df_train_lstm = df_train_lstm.rename(columns={"Date": "ds", "Close": "y"})
 
-m = Prophet()
-m.fit(df_train)
-future = m.make_future_dataframe(periods = period)
-forecast = m.predict(future)
+    # Scaling the data
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    df_train_lstm["y_scaled"] = scaler.fit_transform(df_train_lstm[["y"]])
 
-#Show and plot forecast
-st.subheader("Forecast Data")
-st.write(forecast.tail())
+    # Preparing the data for LSTM input
+    X = df_train_lstm[["ds", "y_scaled"]].values
+    X = np.reshape(X, (X.shape[0], X.shape[1], 1))
 
-st.write(f'Forecast plot for{n_years} years')
-fig1 = plot_plotly(m,forecast)
-st.plotly_chart(fig1)
+    # Building and training the LSTM model
+    model = Sequential()
+    model.add(LSTM(units=50, return_sequences=True, input_shape=(X.shape[1], 1)))
+    model.add(LSTM(units=50))
+    model.add(Dense(1))
+    model.compile(loss="mean_squared_error", optimizer="adam")
+    model.fit(X, df_train_lstm["y_scaled"], epochs=10, batch_size=16, verbose=0)
 
-st.write("Forecast components")
-fig2 = m.plot_components(forecast)
-st.write(fig2)
+    # Predicting with the LSTM model
+    forecast_scaled = model.predict(X)
+    forecast = scaler.inverse_transform(forecast_scaled)
 
+    # Plotting LSTM forecast
+    st.subheader("LSTM Forecast")
+    fig3 = go.Figure()
+    fig3.add_trace(go.Scatter(x=df_train_lstm["ds"], y=df_train_lstm["y"], name="Actual"))
+    fig3.add_trace(go.Scatter(x=df_train_lstm["ds"], y=forecast[:, 0], name="LSTM Forecast"))
+    fig3.layout.update(title_text="LSTM Forecast", xaxis_rangeslider_visible=True)
+    st.plotly_chart(fig3)
+
+else:
+
+    #Predict forecast with Prophet
+    df_train = data[["Date","Close"]]
+    df_train.dropna(inplace=True)
+    df_train = df_train.rename(columns={"Date":"ds","Close":"y"})
+    
+    m = Prophet()
+    m.fit(df_train)
+    future = m.make_future_dataframe(periods = period)
+    forecast = m.predict(future)
+    
+    #Show and plot forecast
+    st.subheader("Forecast Data")
+    st.write(forecast.tail())
+    
+    st.write(f'Forecast plot for{n_years} years')
+    fig1 = plot_plotly(m,forecast)
+    st.plotly_chart(fig1)
+    
+    st.write("Forecast components")
+    fig2 = m.plot_components(forecast)
+    st.write(fig2)
+    
 
 
 
