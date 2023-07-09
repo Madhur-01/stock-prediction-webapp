2cd36d462bb01b1f376f1272598c7166e2a78cea
Madhur-01 <madhurgarg9926@gmail.com>
2023-07-09 22:13:39 +0530

    done

--- main.py
+++ main.py
@@ -146,7 +146,7 @@ else :
         model_fit = model.fit()
         
     else : 
-        model = ARIMA(df_train_arima["y"], order=(p, d, q),(p,d,q,s))
+        model = ARIMA(df_train_arima["y"], order=(p, d, q),seasonal_order=(p,d,q,s))
         model_fit = model.fit()
 
     forecast = model_fit.forecast(steps=len(df_train_arima))
